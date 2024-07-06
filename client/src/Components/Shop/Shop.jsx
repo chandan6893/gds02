e429d4f2442a7284c3eebb51e0afaafbf426a8f3
chandan6893 <krc6893@gmail.com>
2024-07-06 10:50:25 +0530

    product page with pagination

--- client/src/Components/Shop/Shop.jsx
+++ client/src/Components/Shop/Shop.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./Style/Shop.css";
 import axios from "axios";
-import Pagination from '@mui/material/Pagination';
+import Pagination from "@mui/material/Pagination";
+
 
 // ***********************
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  console.log("products",products)
+  console.log("products", products);
   // pagination
   const perPageRecords = 6;
   const totlaPageRecords = products.length;
@@ -21,8 +21,10 @@ const Shop = () => {
 
   const [startPageIndex, setStartPageIndex] = useState(0);
   const [endPageIndex, setEndPageIndex] = useState(perPageRecords - 1);
-
+  const topRef = useRef(null);
   const displayPage = (pageNo) => {
+    
+    scrollToTop();
     const start_page_index = perPageRecords * pageNo - perPageRecords;
     const end_page_index = perPageRecords * pageNo - 1;
 
@@ -35,79 +37,95 @@ const Shop = () => {
     }
   };
 
-  const fetchProduct = () =>{
-    axios.get("https://example-data.draftbit.com/cars?_limit=50")
-    .then(res=>setProducts(res.data))
-    .catch(error=>console.log(error))
-  }
+  const fetchProduct = () => {
+    axios
+      .get("https://example-data.draftbit.com/cars?_limit=51")
+      .then((res) => setProducts(res.data))
+      .catch((error) => console.log(error));
+  };
 
-  useEffect(()=>{
-    fetchProduct()
-  },[])
+  useEffect(() => {
+    fetchProduct();
+  }, []);
   // https://example-data.draftbit.com/sneakers?_limit=10
+  const scrollToTop = () => {
+    // topRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    window.scrollTo(0, 0);
+  };
   return (
-    <div>
-      <div className="Product">
-        {products.length > 0 ? (
-          <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center",gap:"3rem"}}>
-
-            <div className="Products">
-              {(() => {
-                const displayProducts = [];
-                products
-                  .slice(startPageIndex, endPageIndex + 1)
-                  .map((product, ind) => {
-                    displayProducts.push(
-                      <Card sx={{ width: 345 }} key={product.id}>
+    // <div>
+    <div ref={topRef}>
+      {products.length > 0 ? (
+        <div className="Products">
+          {/* style={{display:"flex",flexDirection:"column",justifyContent:"center",gap:"3rem"}} */}
+          <div className="ProductCardContainer">
+            {(() => {
+              const displayProducts = [];
+              products
+                .slice(startPageIndex, endPageIndex + 1)
+                .map((product, ind) => {
+                  displayProducts.push(
+                    <Card sx={{ width: 345 }} key={product.id}>
                       <CardActionArea>
                         <CardMedia
                           component="img"
                           // height="140"
                           height="300"
-                          
-                          // image={product.image}
+                          image={product.image}
                           alt="green iguana"
                         />
                         <CardContent>
-                          <Typography gutterBottom variant="h5" component="div" style={{textAlign:"center"}}>
-                            {/* {product.model} */}
+                          <Typography
+                            gutterBottom
+                            variant="h5"
+                            component="div"
+                            color="#2196f3"
+                            style={{ textAlign: "center" }}
+                          >
+                            {product.model}
                           </Typography>
-                          <Typography variant="body2" color="text.secondary" style={{textAlign:"center"}}>
+                          <Typography
+                            variant="body2"
+                            color="text.secondary"
+                            style={{ textAlign: "center" }}
+                          >
                             {/* Lizards are a widespread group of squamate reptiles, with over 6,000
                             species, ranging across all continents except Antarctica */}
-                           {/* ${product.price} */}
+                            ${product.price}
                           </Typography>
-                          
                         </CardContent>
                       </CardActionArea>
                       <CardActions>
-                        <Button size="lg" fullWidth={true} color="primary" style={{ backgroundColor: '#4CAF50', color: 'white' }} >
+                        <Button
+                          size="lg"
+                          fullWidth={true}
+                          color="primary"
+                          style={{ backgroundColor: "#4CAF50", color: "white" }}
+                        >
                           Add To Cart
                         </Button>
                       </CardActions>
                     </Card>
-                    );
-                  });
+                  );
+                });
 
-                return displayProducts;
-              })()}
-            </div>
-
-            <div className="ProductsPaginationContainer">
-              <Pagination
-                count={totalPages}
-                color="primary"
-                style={{}}
-                onChange={(event, value) => displayPage(value)}
-              />
-            </div>
-
+              return displayProducts;
+            })()}
           </div>
-        ) : (
-          <div>Loading...</div>
-        )}
-      </div>
+
+          <div className="ProductsPaginationContainer">
+          <Pagination
+            count={totalPages}
+            color="primary"
+            onChange={(event, value) => displayPage(value)}
+          />
+          </div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
+    // </div>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot, hydrateRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // SSR
 
